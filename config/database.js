fbda1fd5ff1ab24b14fa466ca758ef48a0e71704
leebaronx3 <leebaronx3@gmail.com>
2021-08-31 13:33:40 +0300

    deployment

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('fullsnackdb', 'root', 'Fullsnatch2207', {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     pool: {
