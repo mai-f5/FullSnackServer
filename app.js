64e69de623ff03320c0f18ebacb1a394133d5e79
leebaronx3 <leebaronx3@gmail.com>
2021-09-01 12:47:12 +0300

    added app trust proxy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var staticData = require('./routes/staticData');
 
 
 var app = express();
-app.enable('trust proxy');
+app.set("trust proxy", 1);
 app.use(cors({
     origin: ['http://localhost:3000', 'https://leebaronx3.github.io'],
     credentials: true
