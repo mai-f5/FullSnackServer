9d8fc08b7f3a9f904625d222d7b08bf657ec35f1
leebaronx3 <leebaronx3@gmail.com>
2021-09-01 17:10:24 +0300

    changed cookie options

--- routes/users.js
+++ routes/users.js
@@ -68,7 +68,7 @@ router.post('/login', async function (req, res, next) {
     const match = await bcrypt.compare(password, user.password);
 
     if (match) {
-      res.cookie('fsCookie', JSON.stringify(user.id), { httpOnly: false })
+      res.cookie('fsCookie', JSON.stringify(user.id), { sameSite: "none", secure: true })
       delete user.dataValues['password']
       res.send(user)
     } else {
